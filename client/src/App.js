be6a84b2877973268bcc1174951d328118a70d68
Ahmed Nasser <ahmednas197@gmail.com>
2020-12-01 21:46:35 +0200

    added the client frontend folder and test it

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react' ; 
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     Hello to the mern stack recipe
+    </div>
+  );
+}
+
+export default App;
