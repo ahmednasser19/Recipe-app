9081948b22391e0e6652a9d4c60238bb8cc99305
Ahmed Nasser <ahmednas197@gmail.com>
2020-12-02 02:52:29 +0200

    added Recipe components
    added Recipe components  and uses hooks and edit the recipes components

---
+++ client/src/components/Recipe.js
@@ -0,0 +1,68 @@
+import React , {useState , useEffect} from 'react'
+import styled from "styled-components" ; 
+import axios from 'axios';
+import spinner from '../spinner.gif';
+import { Link } from 'react-router-dom';
+const Recipe  = (props) => {
+
+    const [title , setTitle] = useState('');
+    const [recipe , setRecipe] = useState('');
+    const [authorname , setAuthorname] = useState('');
+
+    // get the data form DB
+    useEffect(() => {
+        axios.get(`/recipes/${props.match.params.id}`)
+        .then(res => [
+            setTitle(res.data.title),
+            setRecipe(res.data.recipe),
+            setAuthorname(res.data.authorname)
+
+        ])
+        .catch(error => console.log(error));
+    },[props]) ;
+
+
+    return (    
+            <MainContainer>
+                {!title || !recipe ||! authorname ? <img src={spinner} alt="Lodding..."/> : 
+                <>
+                    <h2>{title}</h2>
+                    <p>{recipe}</p>
+                    <p className="badge badge-secondary">{authorname}</p>
+                    <br/>
+                    <Link to="/" type="submit" className="btn btn-primary">Back to Home</Link>
+          
+                </>
+                }
+                
+            </MainContainer>
+    )
+}
+
+export default Recipe
+
+
+///MAIN CONTAINER
+const MainContainer = styled.div`
+        margin : 6rem ; 
+        padding: 3rem 14rem;
+
+        h2{
+            text-align:center;
+            font-weight : 900;
+            color: var(--dark-green)
+        }
+
+        img { 
+            
+            display: block : 
+            margin :auto ; 
+        }
+
+        .btn-primary {
+           
+            background: var(--dark-green);
+            &:hover {
+                background: var(--ligh-green);
+            }        
+`;
--- client/src/components/Recipes.js
+++ client/src/components/Recipes.js
@@ -9,24 +9,28 @@ const Recipes = ({posts }) => {
             posts.map((recipe , key)  => (
                
                 <div className="container" key={key}>
-                <h2>{recipe.title}</h2>
-                <p>{recipe.recipe}</p>
-                <span className="badge badge-secondary p-2">{recipe.authorname}</span>
-                <div className="row my-5">
-                    <div className="col-sm-2">
-                        <Link  to="/edit-recipe" className=" btn btn-outline-success">Edit Recipe </Link>
+                    <Link to={{
+                        pathname : `/recipe/${recipe._id}`                    }}>
+                    <h2>{recipe.title}</h2>
+                    
+                    </Link>
+                    <p>{recipe.recipe}</p>
+                    <span className="badge badge-secondary p-2">{recipe.authorname}</span>
+                    <div className="row my-5">
+                        <div className="col-sm-2">
+                            <Link  to={`/update/${recipe._id}`} className=" btn btn-outline-success">Edit Recipe </Link>
+                        </div>
+                        <div className="col-sm-2">
+                            <button to= "/" className=" btn btn-outline-danger">Delete  Recipe </button>
+                        </div>
                     </div>
-                    <div className="col-sm-2">
-                        <button to= "/" clbuttonssName=" btn btn-outline-danger">Delete  Recipe </button>
-                    </div>
-                </div>
                 </div>
             ))}
         </MainContainer>
     )
 }
 
-export default Recipes
+export default Recipes;
 
 /// constainer 
 const  MainContainer = styled.div`
